abe731face1534cab73331875029e97eb0ae6464
Hotei123 <hotei.mat@gmail.com>
2020-07-24 09:31:48 -0400

    String headerString.

--- test-harness/src/main/java/se/kth/assertteam/jsonbench/Bench.java
+++ test-harness/src/main/java/se/kth/assertteam/jsonbench/Bench.java
@@ -48,6 +48,7 @@ public class Bench {
 			output.delete();
 		}
 		output.createNewFile();
+		String headerString = "Parser,Category,File,Result,MemoryUsed\n";
 		try {
 			Files.write(output.toPath(), "Parser,Category,File,Result,MemoryUsed\n".getBytes(), StandardOpenOption.APPEND);
 		} catch (IOException e) {
@@ -105,9 +106,11 @@ public class Bench {
 		long freeMemory_0;
 		ArrayList<Long> memoryUsedList = resultFile.memoryUsedList;
 		try {
-			freeMemory_0 = runtime.freeMemory();
-			jsonIn = readFile(f);
-			resultFile.memoryUsedList.set(0, freeMemory_0 - runtime.freeMemory());
+			for (int i = 0; i < resultFile.memoryUsedList.size(); i++) {
+				freeMemory_0 = runtime.freeMemory();
+				jsonIn = readFile(f);
+				resultFile.memoryUsedList.set(i, freeMemory_0 - runtime.freeMemory());
+			}
 		} catch (Exception e) {
 			resultFile.kind = ResultKind.FILE_ERROR;
 			return resultFile;
